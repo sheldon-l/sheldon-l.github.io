54ff2b6dc2e87656d8e9d08b44d80daff38c6207
sheldonldev <xiadanli0320@gmail.com>
2020-11-13 16:10:37 +0800

    fix replacement.py

--- replacement.py
+++ replacement.py
@@ -8,19 +8,23 @@ fls = os.listdir(folder)
 
 for file in fls:
 
-  # Print all internal links in files
-  print(file)
-  path = folder + '/' + file
-  with open (path, 'r') as old:
-    lines = old.readlines()
-    new_lines = []
-    for line in lines:
-      if line.find(origin) != -1:
-        line = line.replace(origin, replacement)
-        print(line)
-      new_lines.append(line)
-
+    # print current files name
+    print(file)
+    
+    path = folder + '/' + file
+    with open (path, 'r') as old:
+        lines = old.readlines()
+        new_lines = []
+        for line in lines:
+            if line.find(origin) != -1:
+                line = line.replace(origin, replacement)
+                # print origin content will be replaced, then append to array 
+                print(line)
+            # store all the content should or should not be replaced
+            new_lines.append(line)
+    
+    # save all the content of the file to a new file
     temp_path = './temp/' + file
     with open (temp_path, 'w') as new:
-      for line in new_lines:
-        new.write(line)
+        for line in new_lines:
+            new.write(line)
